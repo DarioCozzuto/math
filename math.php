e902ba4637b3abc9ff64d7eb47b01f1ff1ed0318
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-10 20:47:14 +0100

    Add files via upload

---
+++ math.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+pi();   //p greco
+
+(min(1,2,3,4,5));   //minimo
+(max(1,2,3,4,5));   //massimo
+
+(abs(-6.7));    //valore assoluto=positivo
+
+(sqrt(64));     //radice quadrata
+
+(round(0.49));  //numero intero piu vicino
+
+(rand(1,20));    //random tra 1 e 20
+(rand());       //random tra numeri interi
+
+
+
+?>
+ 
+</body>
+</html>
